481fc66b3574e3f741ed4bfc8ce9b8d3d40fa0cd
richard <richardkovacik.123@gmail.com>
2021-09-20 11:54:29 +0200

    pridanie metody vratNovu v triede Smer

--- src/sk/fri/uniza/Smer.java
+++ src/sk/fri/uniza/Smer.java
@@ -17,6 +17,19 @@ public enum Smer {
     public String getRepre() {
         return repre;
     }
+
+    public Pozicia vratNovuPoziciu(Pozicia stara) {
+        Pozicia novaPos = new Pozicia(stara.getX(), stara.getY());
+
+        switch (this) {
+            case UP -> novaPos.setY(novaPos.getY() + 1);
+            case DOWN -> novaPos.setY(novaPos.getY() - 1);
+            case LEFT -> novaPos.setX(novaPos.getX() - 1);
+            case RIGHT -> novaPos.setX(novaPos.getX() + 1);
+        }
+
+        return novaPos;
+    }
    public static Smer dajNahodnySmer() {
         Smer[] smery = Smer.values();
         Random random = new Random();
