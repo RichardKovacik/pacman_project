996d5b2d9b8b80d970a3fcb2a236e0a72abf10e6
richard <richardkovacik.123@gmail.com>
2021-09-20 11:12:52 +0200

    pridanie metody na vratenie nahodneho smeru

--- src/sk/fri/uniza/Smer.java
+++ src/sk/fri/uniza/Smer.java
@@ -1,2 +1,25 @@
-package sk.fri.uniza;public enum Smer {
+package sk.fri.uniza;
+
+import java.util.Random;
+
+public enum Smer {
+    LEFT("l"),
+    RIGHT("R"),
+    UP("U"),
+    DOWN("D");
+
+    private String repre;
+
+    Smer(String repre) {
+        this.repre = repre;
+    }
+
+    public String getRepre() {
+        return repre;
+    }
+   public Smer dajNahodnySmer() {
+        Smer smery[] = Smer.values();
+        Random random = new Random();
+        return smery[random.nextInt(4)];
+   }
 }
